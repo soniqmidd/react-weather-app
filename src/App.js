9a53dc2215b499866855e970f8c97d6e8309b54c
soniqmidd <soniqmidd@users.noreply.github.com>
2023-11-15 22:02:18 -0600

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-
+import Weather from "./Weather";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project was created by{" "}
           <a
---
+++ src/Weather.js
@@ -0,0 +1,51 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city .."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+
+      <h1>New York </h1>
+      <ul>
+        <li>Wednesday 11:00 pm</li>
+        <li>Clear</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+            alt="Clear"
+          />
+          <span className="temperature">45</span>
+          <span className="unit">°F</span>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 63%</li>
+            <li>Humidity: 5 mph</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
